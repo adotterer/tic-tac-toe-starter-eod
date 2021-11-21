7cb91f15f0bec386865d9602deaa7cfc9ae68e75
Andrew Dotterer <adotterer@gmail.com>
2021-11-21 00:05:14 -0800

    < is more

--- utils.js
+++ utils.js
@@ -5,10 +5,10 @@ export const oURL =
   "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg";
 
 export function smoothElementLoad(ele) {
-  ele.style.opacity = 0.1; // basically transparent 
+  ele.style.opacity = 0.1; // basically transparent
   ele.style.width = "1%";
   setTimeout(() => {
     ele.style.opacity = 1;
     ele.style.width = "85%";
-  }, 80);
+  }, 40);
 }
