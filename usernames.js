9a6b12b40481013e59107bec645f5f0d3f8e7a6d
adotterer <adotterer@gmail.com>
2021-12-17 10:37:19 -0800

    .

--- usernames.js
+++ usernames.js
@@ -18,8 +18,8 @@ const xImg = document.createElement("img");
 xImg.src = xURL;
 const oImg = document.createElement("img");
 oImg.src = oURL;
-xImgSpan.appendChild(xImg);
-oImgSpan.appendChild(oImg);
+xImgSpan.appendChild(smoothElementLoad(xImg));
+oImgSpan.appendChild(smoothElementLoad(oImg));
 export function beginNewGame() {
   let game = new Game(user1.value, user2.value);
   return game.toggleFormGridDisplay();
